56211150e00b1eb743f37df9265da195f12b4eed
Kacper Poniatowski <kacper.krzysztof.poniatowski@estudiantant.upc.edu>
2024-11-30 17:56:59 +0100

    added synset techniques [NOT TESTED FOR RESULTS]

--- project/notebooks/features/statistical_features.py
+++ project/notebooks/features/statistical_features.py
@@ -1,4 +1,9 @@
+from functools import lru_cache
+from itertools import product
+from pandas import DataFrame
 from utils.preprocessor import Preprocessor
+
+# TODO: change this into a class, create preprocessor object in __init__ and use it in the methods
 
 NOUNS = ['NN', 'NNS', 'NNP', 'NNPS']
 VERBS = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
@@ -42,7 +47,7 @@ def add_POS_statistics(df):
     for feature_name, values in relational_features.items():
         df[feature_name] = values
 
-    # NOTE: Changed this because (I think) we don't need to be storing the individual statistics in the dataframe,
+    # NOTE: Changed this because (right now) we don't need to be storing the individual statistics in the dataframe,
     # Only the differences. We cannot use individual statistics for training.
 
     # Add POS statistics
@@ -73,3 +78,68 @@ def add_POS_statistics(df):
     # df['dif_n_adjectives'] = df['s1_n_adjectives'] - df['s2_n_adjectives']
     # df['dif_n_adverbs'] = df['s1_n_adverbs'] - df['s2_n_adverbs']
     # # print(df.loc[0])
+
+
+from nltk.corpus import wordnet
+
+def add_synset_statistics(df: DataFrame):
+    preprop = Preprocessor()
+
+    # Preprocess the DataFrame to extract synsets
+    pos = preprop.preprocess_df(df, 'tokenise_noPunct_lowercase_POS_lemma_noStop_synset')
+
+    relational_features = {
+        'shared_synsets_count': [],
+        'shared_synsets_ratio': [],
+        'avg_synset_similarity': [],
+        'max_synset_similarity': [],
+    }
+
+    @lru_cache(maxsize=None)
+    def cached_wup_similarity(syn1, syn2):
+        return syn1.wup_similarity(syn2)
+
+    for pair in pos:
+
+        print(pair)
+        s1_synsets = [synset_list[:3] for synset_list in pair[0] if synset_list]
+        s2_synsets = [synset_list[:3] for synset_list in pair[1] if synset_list]
+
+        # Flatten and clean synsets for both sentences
+        s1_synsets = [synset for synset_list in pair[0] for synset in synset_list]  # Flatten list of lists
+        s2_synsets = [synset for synset_list in pair[1] for synset in synset_list]  # Flatten list of lists
+
+        # Handle empty synset lists
+        if not s1_synsets or not s2_synsets:
+            relational_features['shared_synsets_count'].append(0)
+            relational_features['shared_synsets_ratio'].append(0)
+            relational_features['avg_synset_similarity'].append(0)
+            relational_features['max_synset_similarity'].append(0)
+            continue
+
+        # Shared synsets
+        shared_synsets = set(s1_synsets).intersection(set(s2_synsets))
+        relational_features['shared_synsets_count'].append(len(shared_synsets))
+
+        # Shared synset ratio (normalized by total unique synsets)
+        total_unique_synsets = len(set(s1_synsets).union(set(s2_synsets)))
+        shared_ratio = len(shared_synsets) / total_unique_synsets if total_unique_synsets > 0 else 0
+        relational_features['shared_synsets_ratio'].append(shared_ratio)
+
+        similarities = [
+            cached_wup_similarity(syn1, syn2)
+            for syn1, syn2 in product(s1_synsets, s2_synsets)
+            if cached_wup_similarity(syn1, syn2) is not None
+        ]
+
+        # Calculate average and maximum similarities
+        if similarities:
+            relational_features['avg_synset_similarity'].append(sum(similarities) / len(similarities))
+            relational_features['max_synset_similarity'].append(max(similarities))
+        else:
+            relational_features['avg_synset_similarity'].append(0)
+            relational_features['max_synset_similarity'].append(0)
+
+    # Add the relational features to the DataFrame
+    for feature_name, values in relational_features.items():
+        df[feature_name] = values
--- project/notebooks/utils/preprocessor.py
+++ project/notebooks/utils/preprocessor.py
@@ -3,32 +3,62 @@ import nltk
 from nltk.corpus import stopwords
 import pandas as pd
 from nltk import pos_tag
+from nltk.corpus import wordnet
 
 class Preprocessor:
     def __init__(self):
         # Different preprocessing pipelines, modify if required
         self.pipelines = {
-            'semantic': [self.lowercase, self.tokenize, self.lemmatize],
-            'jaccard': [self.lowercase, self.remove_punctuation, self.tokenize],
-            'lowest_common_subsequence': [self.lowercase],
-            #'pos_tag_overlap': [self.lowercase, self.POS_tag],
-            'sentence_length_ratio': [self.tokenize],
-            'default': [self.lowercase],
+            # 'semantic': [self.lowercase, self.tokenize, self.lemmatize],
+            # 'jaccard': [self.lowercase, self.remove_punctuation, self.tokenize],
+            # 'lowest_common_subsequence': [self.lowercase],
+            # #'pos_tag_overlap': [self.lowercase, self.POS_tag],
+            # 'sentence_length_ratio': [self.tokenize],
+            # 'default': [self.lowercase],
 
             # Alternative naming for pipelines
-            'lowercase_noPunct_tokenize_onlyWords_noStop_POS': [self.lowercase, self.remove_punctuation, self.tokenize, self.remove_no_words, self.remove_stopwords, self.POS_tag],
+            'lowercase_noPunct_tokenize_onlyWords_noStop_POS': [self.lowercase,
+                                                                self.remove_punctuation,
+                                                                self.tokenize,
+                                                                self.remove_no_words,
+                                                                self.remove_stopwords,
+                                                                self.POS_tag],
+            # Synset creation
+            'tokenise_noPunct_lowercase_POS_lemma_noStop_synset': [self.lowercase,
+                                                                   self.remove_punctuation,
+                                                                self.tokenize,
+                                                                self.remove_stopwords,
+                                                                self.POS_tag,
+                                                                self.lemmatize,
+                                                                self.synset],
         }
         self.stopwords = set(stopwords.words('english'))
 
     def lowercase(self, text: str) -> str:
         return text.lower()
-
+    
     def tokenize(self, text: str) -> list:
         return nltk.word_tokenize(text)
 
+    def get_wordnet_pos(self, treebank_tag):
+        if treebank_tag.startswith('J'):
+            return wordnet.ADJ
+        elif treebank_tag.startswith('V'):
+            return wordnet.VERB
+        elif treebank_tag.startswith('N'):
+            return wordnet.NOUN
+        elif treebank_tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return None
+    
     def lemmatize(self, tokens: list) -> list:
         lemmatizer = nltk.WordNetLemmatizer()
-        return [lemmatizer.lemmatize(token) for token in tokens]
+        lemmatized_tokens = []
+        for token, tag in tokens:
+            wordnet_pos = self.get_wordnet_pos(tag) or wordnet.NOUN  # Default to NOUN if no mapping
+            lemmatized_tokens.append(lemmatizer.lemmatize(token, pos=wordnet_pos))
+        return lemmatized_tokens
 
     def remove_punctuation(self, text: str) -> str:
         return re.sub(r'[^\w\s]', '', text)
@@ -41,6 +71,9 @@ class Preprocessor:
 
     def POS_tag(self, tokens: list) -> list:
         return nltk.pos_tag(tokens)
+    
+    def synset(self, tokens: list) -> list:
+        return [nltk.corpus.wordnet.synsets(token) for token in tokens]
 
     def add_pipeline(self, name: str, steps: list):
         """
@@ -52,26 +85,10 @@ class Preprocessor:
         """
         Apply the specified preprocessing pipeline to the text.
         """
-        pipeline = self.pipelines.get(pipeline_name, self.pipelines['default'])
+        pipeline = self.pipelines.get(pipeline_name)
         for step in pipeline:
             text = step(text)
         return text
-
-    def preprocess_files(self, sentence_files: pd.DataFrame, pipeline_mapping: dict) -> pd.DataFrame:
-        """
-        Preprocess content in the DataFrame based on the pipeline mapping.
-        """
-        for idx, row in sentence_files.iterrows():
-            filename = row['filename']
-
-            # Determine pipeline based on file type or filename pattern
-            pipeline_name = pipeline_mapping.get(filename.split('_')[0], 'default')
-
-            # Apply preprocessing
-            content = row['content']
-            sentence_files.at[idx, 'content'] = self.preprocess(content, pipeline_name)
-
-        return sentence_files
     
     def preprocess_df(self, df: pd.DataFrame, pipeline_name: str) -> any:
         """
@@ -85,3 +102,20 @@ class Preprocessor:
         return ret
     
 
+
+
+    # def preprocess_files(self, sentence_files: pd.DataFrame, pipeline_mapping: dict) -> pd.DataFrame:
+    #     """
+    #     Preprocess content in the DataFrame based on the pipeline mapping.
+    #     """
+    #     for idx, row in sentence_files.iterrows():
+    #         filename = row['filename']
+
+    #         # Determine pipeline based on file type or filename pattern
+    #         pipeline_name = pipeline_mapping.get(filename.split('_')[0], 'default')
+
+    #         # Apply preprocessing
+    #         content = row['content']
+    #         sentence_files.at[idx, 'content'] = self.preprocess(content, pipeline_name)
+
+    #     return sentence_files
