1df9baf5c82cca9bd610dc3231201179aa5ca24c
Pau Blanco <pblanco@knapptic.com>
2024-12-03 12:56:56 +0100

    Features by verb tense. New models

--- project/notebooks/features/statistical_features.py
+++ project/notebooks/features/statistical_features.py
@@ -11,74 +11,81 @@ VERBS = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 ADJECTIVES = ['JJ', 'JJR', 'JJS']
 ADVERBS = ['RB', 'RBR', 'RBS']
 
+VERBS_PRESENT = ['VBG', 'VBP', 'VBZ']
+VERBS_PAST = ['VBD', 'VBN']
+VERBS_OTHER = ['VB']
+
 def add_POS_statistics(df):
+    print("Adding POS based features...")
     # Create a preprocessor object
     preprop = Preprocessor()
     # Preprocess and extract POS tags for each sentence
     pos = preprop.preprocess_df(df, 'lowercase_noPunct_tokenize_onlyWords_noStop_POS')
 
-    relational_features = {
-        'dif_n_words': [],
-        'dif_n_verbs': [],
-        'dif_n_nouns': [],
-        'dif_n_adjectives': [],
-        'dif_n_adverbs': []
-    }
+    # relational_features = {
+    #     'dif_n_words': [],
+    #     'dif_n_verbs': [],
+    #     'dif_n_nouns': [],
+    #     'dif_n_adjectives': [],
+    #     'dif_n_adverbs': []
+    # }
 
     # Compute relational statistics for each sentence pair
-    for pair in pos:
-        s1 = pair[0]  
-        s2 = pair[1]
+    # for pair in pos:
+    #     s1 = pair[0]  
+    #     s2 = pair[1]
         
-        # Relational features: differences
-        relational_features['dif_n_words'].append(len(s1) - len(s2))
-        relational_features['dif_n_verbs'].append(
-            len([word for word in s1 if word[1] in VERBS]) - len([word for word in s2 if word[1] in VERBS])
-        )
-        relational_features['dif_n_nouns'].append(
-            len([word for word in s1 if word[1] in NOUNS]) - len([word for word in s2 if word[1] in NOUNS])
-        )
-        relational_features['dif_n_adjectives'].append(
-            len([word for word in s1 if word[1] in ADJECTIVES]) - len([word for word in s2 if word[1] in ADJECTIVES])
-        )
-        relational_features['dif_n_adverbs'].append(
-            len([word for word in s1 if word[1] in ADVERBS]) - len([word for word in s2 if word[1] in ADVERBS])
-        )
+    #     # Relational features: differences
+    #     relational_features['dif_n_words'].append(len(s1) - len(s2))
+    #     relational_features['dif_n_verbs'].append(
+    #         len([word for word in s1 if word[1] in VERBS]) - len([word for word in s2 if word[1] in VERBS])
+    #     )
+    #     relational_features['dif_n_nouns'].append(
+    #         len([word for word in s1 if word[1] in NOUNS]) - len([word for word in s2 if word[1] in NOUNS])
+    #     )
+    #     relational_features['dif_n_adjectives'].append(
+    #         len([word for word in s1 if word[1] in ADJECTIVES]) - len([word for word in s2 if word[1] in ADJECTIVES])
+    #     )
+    #     relational_features['dif_n_adverbs'].append(
+    #         len([word for word in s1 if word[1] in ADVERBS]) - len([word for word in s2 if word[1] in ADVERBS])
+    #     )
 
-    for feature_name, values in relational_features.items():
-        df[feature_name] = values
+    # for feature_name, values in relational_features.items():
+    #     df[feature_name] = values
 
-    # NOTE: Changed this because (right now) we don't need to be storing the individual statistics in the dataframe,
-    # Only the differences. We cannot use individual statistics for training.
 
     # Add POS statistics
-    # for i in range(len(pos)):
-    #     # Number of words
-    #     df.loc[i, 's1_n_words'] = len(pos[i][0])
-    #     df.loc[i, 's2_n_words'] = len(pos[i][1])
-    #     # Number of verbs
-    #     df.loc[i, 's1_n_verbs'] = len([word for word in pos[i][0] if word[1] in VERBS])
-    #     df.loc[i, 's2_n_verbs'] = len([word for word in pos[i][1] if word[1] in VERBS])
-    #     # Number of nouns
-    #     df.loc[i, 's1_n_nouns'] = len([word for word in pos[i][0] if word[1] in NOUNS])
-    #     df.loc[i, 's2_n_nouns'] = len([word for word in pos[i][1] if word[1] in NOUNS])
-    #     # Number of adjectives
-    #     df.loc[i, 's1_n_adjectives'] = len([word for word in pos[i][0] if word[1] in ADJECTIVES])
-    #     df.loc[i, 's2_n_adjectives'] = len([word for word in pos[i][1] if word[1] in ADJECTIVES])
-    #     # Number of adverbs
-    #     df.loc[i, 's1_n_adverbs'] = len([word for word in pos[i][0] if word[1] in ADVERBS])
-    #     df.loc[i, 's2_n_adverbs'] = len([word for word in pos[i][1] if word[1] in ADVERBS])
-    #     # print(df.loc[i])
-    #     # print("S1: ", pos[i][0])
-    #     # print("S2: ", pos[i][1])
+    for i in range(len(pos)):
+        # Number of words
+        df.loc[i, 's1_n_words'] = len(pos[i][0])
+        df.loc[i, 's2_n_words'] = len(pos[i][1])
+        # Number of verbs
+        df.loc[i, 's1_n_verbs_tot'] = len([word for word in pos[i][0] if word[1] in VERBS])
+        df.loc[i, 's2_n_verbs_tot'] = len([word for word in pos[i][1] if word[1] in VERBS])
+        # Verbs in present
+        df.loc[i, 's1_n_verbs_pres'] = len([word for word in pos[i][0] if word[1] in VERBS_PRESENT])
+        df.loc[i, 's2_n_verbs_pres'] = len([word for word in pos[i][1] if word[1] in VERBS_PRESENT])
+        # Verbs in present
+        df.loc[i, 's1_n_verbs_past'] = len([word for word in pos[i][0] if word[1] in VERBS_PAST])
+        df.loc[i, 's2_n_verbs_past'] = len([word for word in pos[i][1] if word[1] in VERBS_PAST])
+        # Number of nouns
+        df.loc[i, 's1_n_nouns'] = len([word for word in pos[i][0] if word[1] in NOUNS])
+        df.loc[i, 's2_n_nouns'] = len([word for word in pos[i][1] if word[1] in NOUNS])
+        # Number of adjectives
+        df.loc[i, 's1_n_adjectives'] = len([word for word in pos[i][0] if word[1] in ADJECTIVES])
+        df.loc[i, 's2_n_adjectives'] = len([word for word in pos[i][1] if word[1] in ADJECTIVES])
+        # Number of adverbs
+        df.loc[i, 's1_n_adverbs'] = len([word for word in pos[i][0] if word[1] in ADVERBS])
+        df.loc[i, 's2_n_adverbs'] = len([word for word in pos[i][1] if word[1] in ADVERBS])
 
-    # # Compute also the differences
-    # df['dif_n_words'] = df['s1_n_words'] - df['s2_n_words']
-    # df['dif_n_verbs'] = df['s1_n_verbs'] - df['s2_n_verbs']
-    # df['dif_n_nouns'] = df['s1_n_nouns'] - df['s2_n_nouns']
-    # df['dif_n_adjectives'] = df['s1_n_adjectives'] - df['s2_n_adjectives']
-    # df['dif_n_adverbs'] = df['s1_n_adverbs'] - df['s2_n_adverbs']
-    # # print(df.loc[0])
+    # Compute also the differences
+    df['dif_n_words'] = df['s1_n_words'] - df['s2_n_words']
+    df['dif_n_verbs_tot'] = df['s1_n_verbs_tot'] - df['s2_n_verbs_tot']
+    df['dif_n_verbs_pres'] = df['s1_n_verbs_pres'] - df['s2_n_verbs_pres']
+    df['dif_n_verbs_past'] = df['s1_n_verbs_past'] - df['s2_n_verbs_past']
+    df['dif_n_nouns'] = df['s1_n_nouns'] - df['s2_n_nouns']
+    df['dif_n_adjectives'] = df['s1_n_adjectives'] - df['s2_n_adjectives']
+    df['dif_n_adverbs'] = df['s1_n_adverbs'] - df['s2_n_adverbs']
 
 
 from nltk.corpus import wordnet
@@ -160,7 +167,7 @@ def compute_sysnsets_distances(df, row, prefix, synsets1, synsets2):
     similarities = [
         cached_wup_similarity(syn1, syn2)
         for syn1, syn2 in product(synsets1, synsets2)
-        if cached_wup_similarity(syn1, syn2) is not None
+        if syn1.pos() == syn2.pos() and cached_wup_similarity(syn1, syn2) is not None
     ]
 
     df.loc[row, prefix + 'shared_synsets_count'] = len(shared_synsets)
@@ -169,6 +176,8 @@ def compute_sysnsets_distances(df, row, prefix, synsets1, synsets2):
     df.loc[row, prefix + 'max_synset_similarity'] = max(similarities) if similarities else -1
 
 def add_synset_statistics_ext(df: DataFrame):
+    print("Adding synset based features...")
+
     preprop = Preprocessor()
 
     # Preprocess the DataFrame to extract synsets
@@ -196,5 +205,5 @@ def add_synset_statistics_ext(df: DataFrame):
         if i % 10 == 0:
             print(f"\rProcessed {i} of {total} rows ({i*100/total:.1f}%)", end='', flush=True)
     
-    print(f"\rProcessed {i + 1}/{len(syns)} rows (100.0%)")
+    print(f"\rProcessed {i + 1}/{len(syns)} rows (100.0%)    ")
 
